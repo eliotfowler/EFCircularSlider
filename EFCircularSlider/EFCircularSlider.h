192cd4dee2239450d9c24dfb3d47faa28af55339
Eliot Fowler <jfowler@cardinalsolutions.com>
2013-12-04 16:50:16 -0500

    cleanup and switching to properties over private variables and methods

--- EFCircularSlider/EFCircularSlider.h
+++ EFCircularSlider/EFCircularSlider.h
@@ -10,16 +10,23 @@
 
 @interface EFCircularSlider : UIControl
 
-@property (nonatomic) float minimumValue;
-@property (nonatomic) float maximumValue;
-@property (nonatomic) float currentValue;
-
 typedef enum : NSUInteger {
     semiTransparentWhiteCircle,
     semiTransparentBlackCircle,
     doubleCircleWithOpenCenter,
     doubleCircleWithClosedCenter
 } HandleType;
+
+@property (nonatomic) float minimumValue;
+@property (nonatomic) float maximumValue;
+@property (nonatomic) float currentValue;
+@property (nonatomic) int lineWidth;
+@property (nonatomic, strong) UIColor* handleColor;
+@property (nonatomic, strong) UIFont* labelFont;
+@property (nonatomic) BOOL snapToLabels;
+@property (nonatomic, strong) UIColor* filledColor;
+@property (nonatomic, strong) UIColor* unfilledColor;
+@property (nonatomic) HandleType handleType;
 
 -(void)setHandleColor:(UIColor*)color;
 -(void)setInnerMarkingLabels:(NSArray*)labels;
--- EFCircularSlider/EFCircularSlider.m
+++ EFCircularSlider/EFCircularSlider.m
@@ -15,17 +15,10 @@
 
 @implementation EFCircularSlider {
     CGFloat radius;
-    CGFloat lineWidth;
     int angle;
     int fixedAngle;
-    UIColor* handleColor;
     NSMutableDictionary* labelsWithPercents;
     NSArray* labelsEvenSpacing;
-    UIFont* labelFont;
-    BOOL snapToLabels;
-    UIColor* filledColor;
-    UIColor* unfilledColor;
-    int handleType;
 }
 
 - (id)initWithFrame:(CGRect)frame
@@ -38,16 +31,15 @@
         _currentValue = 0.0f;
         angle = 0;
         
-        lineWidth = 5;
-        radius = self.frame.size.height/2 - lineWidth/2 - 10;
-        unfilledColor = [UIColor blueColor];
-        filledColor = [UIColor redColor];
-        handleColor = [UIColor redColor];
-        labelFont = [UIFont systemFontOfSize:10.0f];
+        _lineWidth = 5;
+        radius = self.frame.size.height/2 - _lineWidth/2 - 10;
+        _unfilledColor = [UIColor blueColor];
+        _filledColor = [UIColor redColor];
+        _handleColor = [UIColor redColor];
+        _labelFont = [UIFont systemFontOfSize:10.0f];
         labelsEvenSpacing = @[@"12", @"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"11"];
-//        labelsEvenSpacing = @[@"12", @"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"11", @"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"11"];
-        snapToLabels = YES;
-        handleType = doubleCircleWithOpenCenter;
+        _snapToLabels = YES;
+        _handleType = doubleCircleWithOpenCenter;
         
         self.backgroundColor = [UIColor clearColor];
     }
@@ -64,20 +56,20 @@
     
     //Draw the unfilled circle
     CGContextAddArc(ctx, self.frame.size.width/2, self.frame.size.height/2, radius, 0, M_PI *2, 0);
-    [unfilledColor setStroke];
-    CGContextSetLineWidth(ctx, lineWidth);
+    [_unfilledColor setStroke];
+    CGContextSetLineWidth(ctx, _lineWidth);
     CGContextSetLineCap(ctx, kCGLineCapButt);
     CGContextDrawPath(ctx, kCGPathStroke);
     
     
     //Draw the filled circle
-    if(handleType == doubleCircleWithClosedCenter || handleType == doubleCircleWithOpenCenter) {
+    if(_handleType == doubleCircleWithClosedCenter || _handleType == doubleCircleWithOpenCenter) {
         CGContextAddArc(ctx, self.frame.size.width/2  , self.frame.size.height/2, radius, 3*M_PI/2, 3*M_PI/2-ToRad(angle+5), 0);
     } else {
         CGContextAddArc(ctx, self.frame.size.width/2  , self.frame.size.height/2, radius, 3*M_PI/2, 3*M_PI/2-ToRad(angle), 0);
     }
-    [filledColor setStroke];
-    CGContextSetLineWidth(ctx, lineWidth);
+    [_filledColor setStroke];
+    CGContextSetLineWidth(ctx, _lineWidth);
     CGContextSetLineCap(ctx, kCGLineCapButt);
     CGContextDrawPath(ctx, kCGPathStroke);
     
@@ -93,27 +85,27 @@
 -(void) drawHandle:(CGContextRef)ctx{
     CGContextSaveGState(ctx);
     CGPoint handleCenter =  [self pointFromAngle: angle];
-    if(handleType == semiTransparentWhiteCircle) {
+    if(_handleType == semiTransparentWhiteCircle) {
         [[UIColor colorWithWhite:1.0 alpha:0.7] set];
-        CGContextFillEllipseInRect(ctx, CGRectMake(handleCenter.x, handleCenter.y, lineWidth, lineWidth));
-    } else if(handleType == semiTransparentBlackCircle) {
+        CGContextFillEllipseInRect(ctx, CGRectMake(handleCenter.x, handleCenter.y, _lineWidth, _lineWidth));
+    } else if(_handleType == semiTransparentBlackCircle) {
         [[UIColor colorWithWhite:0.0 alpha:0.7] set];
-        CGContextFillEllipseInRect(ctx, CGRectMake(handleCenter.x, handleCenter.y, lineWidth, lineWidth));
-    } else if(handleType == doubleCircleWithClosedCenter) {
-        [handleColor set];
-        CGContextAddArc(ctx, handleCenter.x - (lineWidth-10)/2, handleCenter.y + (lineWidth)/2, 8, 0, M_PI *2, 0);
-        CGContextSetLineWidth(ctx, lineWidth-2);
+        CGContextFillEllipseInRect(ctx, CGRectMake(handleCenter.x, handleCenter.y, _lineWidth, _lineWidth));
+    } else if(_handleType == doubleCircleWithClosedCenter) {
+        [_handleColor set];
+        CGContextAddArc(ctx, handleCenter.x - (_lineWidth-10)/2, handleCenter.y + (_lineWidth)/2, 8, 0, M_PI *2, 0);
+        CGContextSetLineWidth(ctx, _lineWidth-2);
         CGContextSetLineCap(ctx, kCGLineCapButt);
         CGContextDrawPath(ctx, kCGPathStroke);
-        CGContextFillEllipseInRect(ctx, CGRectMake(handleCenter.x, handleCenter.y, lineWidth, lineWidth));
-    } else if(handleType == doubleCircleWithOpenCenter) {
-        [handleColor set];
-        CGContextAddArc(ctx, handleCenter.x - (lineWidth-10)/2, handleCenter.y + (lineWidth)/2, 8, 0, M_PI *2, 0);
-        CGContextSetLineWidth(ctx, lineWidth-2);
+        CGContextFillEllipseInRect(ctx, CGRectMake(handleCenter.x, handleCenter.y, _lineWidth, _lineWidth));
+    } else if(_handleType == doubleCircleWithOpenCenter) {
+        [_handleColor set];
+        CGContextAddArc(ctx, handleCenter.x - (_lineWidth-10)/2, handleCenter.y + (_lineWidth)/2, 8, 0, M_PI *2, 0);
+        CGContextSetLineWidth(ctx, _lineWidth-2);
         CGContextSetLineCap(ctx, kCGLineCapButt);
         CGContextDrawPath(ctx, kCGPathStroke);
         
-        CGContextAddArc(ctx, handleCenter.x + lineWidth/2, handleCenter.y + lineWidth/2, lineWidth/2, 0, M_PI *2, 0);
+        CGContextAddArc(ctx, handleCenter.x + _lineWidth/2, handleCenter.y + _lineWidth/2, _lineWidth/2, 0, M_PI *2, 0);
         CGContextSetLineWidth(ctx, 1);
         CGContextSetLineCap(ctx, kCGLineCapButt);
         CGContextDrawPath(ctx, kCGPathStroke);
@@ -127,9 +119,9 @@
         return;
     } else {
         NSString* firstLabel = [labelsEvenSpacing objectAtIndex:0];
-        CGFloat firstLabelXPos = self.frame.size.width/2 - [self widthOfString:firstLabel withFont:labelFont]/2;
-        CGRect firstLabelLocation = CGRectMake(firstLabelXPos, lineWidth + 15, [self widthOfString:firstLabel withFont:labelFont], [self heightOfString:firstLabel withFont:labelFont]);
-        NSDictionary *attributes = @{ NSFontAttributeName: labelFont,
+        CGFloat firstLabelXPos = self.frame.size.width/2 - [self widthOfString:firstLabel withFont:_labelFont]/2;
+        CGRect firstLabelLocation = CGRectMake(firstLabelXPos, _lineWidth + 15, [self widthOfString:firstLabel withFont:_labelFont], [self heightOfString:firstLabel withFont:_labelFont]);
+        NSDictionary *attributes = @{ NSFontAttributeName: _labelFont,
                                       NSForegroundColorAttributeName: [UIColor redColor]};
         [firstLabel drawInRect:firstLabelLocation withAttributes:attributes];
         
@@ -138,7 +130,7 @@
             CGFloat percentageAlongCircle = i/(float)[labelsEvenSpacing count];
             CGFloat degreesForLabel = percentageAlongCircle * 360;
             CGPoint closestPointOnCircleToLabel = [self pointFromAngle:degreesForLabel];
-            CGRect labelLocation = CGRectMake(closestPointOnCircleToLabel.x, closestPointOnCircleToLabel.y, [self widthOfString:label withFont:labelFont], [self heightOfString:firstLabel withFont:labelFont]);
+            CGRect labelLocation = CGRectMake(closestPointOnCircleToLabel.x, closestPointOnCircleToLabel.y, [self widthOfString:label withFont:_labelFont], [self heightOfString:firstLabel withFont:_labelFont]);
             int distanceToMove = -15;
             CGPoint centerPoint = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
             float radiansTowardsCenter = ToRad(AngleFromNorth(centerPoint, closestPointOnCircleToLabel, NO));
@@ -169,7 +161,7 @@
 
 -(void)endTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event{
     [super endTrackingWithTouch:touch withEvent:event];
-    if(snapToLabels) {
+    if(_snapToLabels) {
         CGPoint bestGuessPoint;
         float minDist = 360;
         for (int i=0; i<[labelsEvenSpacing count]; i++) {
@@ -192,7 +184,6 @@
     int currentAngle = floor(AngleFromNorth(centerPoint, point, NO));
     angle = 360 - 90 - currentAngle;
     _currentValue = [self valueFromAngle];
-    NSLog(@"Current value is %d", (int)_currentValue);
     [self setNeedsDisplay];
 }
 
@@ -201,7 +192,7 @@
 -(CGPoint)pointFromAngle:(int)angleInt{
     
     //Define the Circle center
-    CGPoint centerPoint = CGPointMake(self.frame.size.width/2 - lineWidth/2, self.frame.size.height/2 - lineWidth/2);
+    CGPoint centerPoint = CGPointMake(self.frame.size.width/2 - _lineWidth/2, self.frame.size.height/2 - _lineWidth/2);
     
     //Define The point position on the circumference
     CGPoint result;
@@ -214,7 +205,7 @@
 -(CGPoint)pointFromAngle:(int)angleInt withObjectSize:(CGSize)size{
     
     //Define the Circle center
-    CGPoint centerPoint = CGPointMake(self.frame.size.width/2 - lineWidth/2, self.frame.size.height/2 - lineWidth/2);
+    CGPoint centerPoint = CGPointMake(self.frame.size.width/2 - _lineWidth/2, self.frame.size.height/2 - _lineWidth/2);
     
     //Define The point position on the circumference
     CGPoint result;
@@ -246,32 +237,18 @@ static inline float AngleFromNorth(CGPoint p1, CGPoint p2, BOOL flipped) {
 
 - (CGFloat) widthOfString:(NSString *)string withFont:(UIFont*)font {
     NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:font, NSFontAttributeName, nil];
-//    NSLog(@"width of string is %f", [[[NSAttributedString alloc] initWithString:string attributes:attributes] size].width);
     return [[[NSAttributedString alloc] initWithString:string attributes:attributes] size].width;
 }
 
 - (CGFloat) heightOfString:(NSString *)string withFont:(UIFont*)font {
     NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:font, NSFontAttributeName, nil];
-//    NSLog(@"height of string is %f", [[[NSAttributedString alloc] initWithString:string attributes:attributes] size].height);
     return [[[NSAttributedString alloc] initWithString:string attributes:attributes] size].height;
 }
 
 #pragma mark - public methods
--(void)setHandleColor:(UIColor *)color {
-    handleColor = color;
-}
-
 -(void)setInnerMarkingLabels:(NSArray*)labels{
     labelsEvenSpacing = labels;
     [self setNeedsDisplay];
-}
-
--(void)setFilledColor:(UIColor *)color {
-    filledColor = color;
-}
-
--(void)setUnfilledColor:(UIColor *)color {
-    unfilledColor = color;
 }
 
 @end
